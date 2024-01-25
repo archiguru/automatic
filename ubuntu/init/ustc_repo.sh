898bea21c874837c39388f7ce2f126b097ae35c1
Jonny.Chang <jonny6015@icloud.com>
2024-01-25 23:41:10 +0800

    update

--- ubuntu/init/ustc_repo.sh
+++ ubuntu/init/ustc_repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
 # Ubuntu 源
-sudo sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
+sudo sed -i 's@//.*.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
 sudo sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 sudo sed -i 's/http:/https:/g' /etc/apt/sources.list
 sudo apt-get update -y
