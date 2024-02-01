ee0866cb7b660177858d09fdc23002dda35e4ee5
Jonny.Chang <jonny6015@icloud.com>
2024-02-01 23:55:00 +0800

    update

---
+++ ubuntu/oracle/oracle_sys_init.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+sudo sed -i "s/\$nrconf{restart}/\$nrconf{restart} = 'a';/g" /etc/needrestart/needrestart.conf
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/^GSSAPIAuthentication yes$/GSSAPIAuthentication no/' /etc/ssh/sshd_config
+sudo sed -i 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
+sudo cat /dev/null >/etc/legal
+sudo tee -a /etc/ssh/sshd_config <<EOF
+PrintLastLog no
+PermitRootLogin yes
+PubkeyAuthentication yes
+TCPKeepAlive yes
+ClientAliveInterval 60
+ClientAliveCountMax 3600
+EOF
+sudo cat >"/home/ubuntu/.ssh/authorized_keys" <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB5AKSY8s8J5gq+XRhN46ccIBKt9TSk4XC+1o+UjwzPt MacBot
+EOF
+sudo cat >"/root/.ssh/authorized_keys" <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB5AKSY8s8J5gq+XRhN46ccIBKt9TSk4XC+1o+UjwzPt MacBot
+EOF
+sudo sed -i "s/\$nrconf{restart}/\$nrconf{restart} = 'a';/g" /etc/needrestart/needrestart.conf
+sudo systemctl restart sshd.service
+sudo ufw disable
+sudo cp -rf /etc/update-motd.d /etc//update-motd.d.backup
+sudo rm -rf /etc/update-motd.d/*
+sudo NEEDRESTART_MODE=a apt-get dist-upgrade --yes
+sudo apt-get update -y
+sudo apt-get install build-essential zsh screen zip unzip wget rsync bzip2 lsof telnet git tree htop vim net-tools universal-ctags apt-transport-https ca-certificates curl software-properties-common gcc gcc-11-locales debian-keyring autoconf automake make libtool flex bison gdb bzr libgd-tools expect lrzsz chrony gsmartcontrol hwloc psmisc -y
+timedatectl set-local-rtc 1
+timedatectl set-timezone Asia/Shanghai
+systemctl start chrony
+systemctl enable chrony
+
+sudo echo "export HISTTIMEFORMAT=\"%F %T \"" >>/etc/profile
+sudo echo "" >>/etc/profile
+sudo echo "alias c=clear" >>/etc/profile
+sudo echo "alias vi=vim" >>/etc/profile
+sudo echo "alias dsh='du -hsx * | sort -rh | head -n 10'" >>/etc/profile
+sudo sed -i 's/HISTSIZE=1000/HISTSIZE=10000/g' /etc/profile
+source /etc/profile
+
+cat >>/etc/security/limits.conf <<EOF
+*           soft   nofile       65535
+*           hard   nofile       65535
+EOF
